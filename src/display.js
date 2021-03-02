0addd66fddfdb2b75aff70b90f32a04d208f6cc6
Michael Lai <mrlai7782@gmail.com>
2021-03-01 22:03:19 -0800

    update

--- src/display.js
+++ src/display.js
@@ -2,12 +2,21 @@ export function display(data) {
   console.log(data);
   document.querySelector(".current-container").innerHTML = `
     <div class="current-city">${data.current.name}</div>
-    <div class="current-temp">${data.current.temp}</div>
     <div class="current-weather">${data.current.weather}</div>
+    <div class="current-temp">${data.current.temp}&deg</div>
     <div class="high-low-container">
-      <div class="current-high">High: ${data.current.high}</div>
-      <div class="current-low">Low: ${data.current.low}</div>
+      <div class="current-high">H: ${data.current.high}&deg</div>
+      <div class="current-low">L: ${data.current.low}&deg</div>
     </div>
-
   `;
+  document.querySelector(".hourly-container").innerHTML = "";
+  for (let i = 0; i < data.hourly.length; i++) {
+    document.querySelector(".hourly-container").innerHTML += `
+  <div class="hourly">
+    <div class="hourly-time">${data.hourly[i].time}</div>
+    <div class="hourly-weather">${data.hourly[i].weather}</div>
+    <div class="hourly-temp">${data.hourly[i].temp}&deg</div>
+  </div>
+`;
+  }
 }
